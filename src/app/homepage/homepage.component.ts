e0d6cbcc2ae5f4b372b891ed9cb64603dffc5960
BenBarnhill <bconnorbarnhill@gmail.com>
2022-03-10 14:22:22 -0600

    changed the login/homepage layout

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../user/auth.service';
+import { User } from '../user/user.model';
 
 @Component({
   selector: 'app-homepage',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomepageComponent implements OnInit {
 
-  constructor() { }
+  newUser: User = {
+    id: 0,
+    password: "",
+    firstName: "",
+    lastName: "",
+    contact: 0,
+    email: "",
+    address: "",
+    role: ""
+  }
+
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
+    this.newUser = this.authService.retrieveUser();
   }
 
 }
